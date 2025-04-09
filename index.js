e6ac3f60e6cc2e5504d32a12d964f634f08e614a
SSARDINIA <shairabeiges@gmail.com>
2025-04-09 13:43:36 +0100

    working on mustache templates

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 const express = require("express");
 const path = require("path");
-// const public = public.join(__dirname, "public");
+const public = path.join(__dirname, "public");
 const bodyParser = require("body-parser");
 const mainRouter = require("./src/routes/mainRoutes");
 const adminRouter = require("./src/routes/adminRoutes");
 const errorRoutes = require("./src/routes/errorRoutes");
-
+const mustache = require("mustache-express");
 const app = express();
 
-const mustache = require("mustache-express");
-
 app.engine("mustache", mustache());
-
+app.set("view engine", "mustache");
+app.set("views", path.join(__dirname, "src", "views"));
 app.set("view options", {
   partials: path.join(__dirname, "src", "views", "layouts"),
 });
 
-app.set("views", path.join(__dirname, "src", "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 
-app.set("view engine", "mustache");
-
-// app.use(express.static(public));
+app.use(express.static(public));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", mainRouter);
 app.use("/admin", adminRouter);
-
-app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 
 app.use(errorRoutes);
 
